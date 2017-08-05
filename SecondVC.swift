ef19f38e238099f517fc1c393f82de0c7aed84d9
raian <you@example.com>
2017-08-05 16:39:45 -0400

    display image from URL

--- SecondVC.swift
+++ SecondVC.swift
@@ -10,10 +10,15 @@ import UIKit
 
 class SecondVC: UIViewController {
 
+    @IBOutlet weak var mLabel: UILabel!
+    @IBOutlet weak var mImageView: UIImageView!
+    
+    var data : String!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        print("data: \(data!)")
         // Do any additional setup after loading the view.
+        mLabel.text = data != nil ? data : "Someting to display"
     }
 
     override func didReceiveMemoryWarning() {
@@ -32,4 +37,31 @@ class SecondVC: UIViewController {
     }
     */
 
+    @IBAction func displayImageFromURL(_ sender: Any) {
+        print("Begin of code")
+        if let checkedUrl = URL(string: "http://www.apple.com/euro/ios/ios8/a/generic/images/og.png") {
+            mImageView.contentMode = .scaleAspectFit
+            downloadImage(url: checkedUrl)
+        }
+        print("End of code. The image will continue downloading in the background and it will be loaded when it ends.")
+    }
+    
+    func getDataFromUrl(url: URL, completion: @escaping (_ data: Data?, _  response: URLResponse?, _ error: Error?) -> Void) {
+        URLSession.shared.dataTask(with: url) {
+            (data, response, error) in
+            completion(data, response, error)
+            }.resume()
+    }
+    
+    func downloadImage(url: URL) {
+        print("Download Started")
+        getDataFromUrl(url: url) { (data, response, error)  in
+            guard let data = data, error == nil else { return }
+            print(response?.suggestedFilename ?? url.lastPathComponent)
+            print("Download Finished")
+            DispatchQueue.main.async() { () -> Void in
+                self.mImageView.image = UIImage(data: data)
+            }
+        }
+    }
 }
--- SimplePassingDataNavigation/ViewController.swift
+++ SimplePassingDataNavigation/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mLabel: UITextField!
+    @IBOutlet weak var mButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func sendData(_ sender: Any) {
+        let secondVC = storyboard?.instantiateViewController(withIdentifier: "SecondVC") as! SecondVC
+        secondVC.data = mLabel.text != nil ? mLabel.text : "Default value"
+        navigationController?.pushViewController(secondVC, animated: true)
+        
+    }
+    
+    
+    
 }
 
